21e348767db37362fccd8a1e55d266dbd649166d
choguun <29241145+choguun@users.noreply.github.com>
2024-08-19 07:26:07 +0700

    Update 01_deploy_protocol.ts

--- hardhat/scripts/01_deploy_protocol.ts
+++ hardhat/scripts/01_deploy_protocol.ts
@@ -4,13 +4,15 @@ import hre from "hardhat";
 async function main() {
   const owner = "0x1a46582A48a04c67D78e062E0631cDDE9fD2DF0a";
 
-  const profile = await hre.viem.deployContract("Profile", [owner]);
   const world = await hre.viem.deployContract("World", [owner]);
+  const randomOracle = await hre.viem.deployContract("SubscriptionConsumer", [111050927125045824511614274940509035041704935106337038931065260344527599144970, world.address]);
+  const profile = await hre.viem.deployContract("Profile", [owner]);
   const token = await hre.viem.deployContract("Token", [owner, world.address, profile.address]);
   const craft = await hre.viem.deployContract("CraftSystem", [owner, world.address]);
   const item = await hre.viem.deployContract("Item", [owner, world.address, craft.address, ""]);
   const vault = await hre.viem.deployContract("ERC4626Vault", [token.address]);
-  const oracle = await hre.viem.deployContract("DataConsumerV3");
+  const dataOracle = await hre.viem.deployContract("DataConsumerV3");
+
   // const erc6551Account  = await hre.viem.deployContract("ERC6551Account");
   // const erc6551Registry = await hre.viem.deployContract("ERC6551Registry");
 
@@ -38,7 +40,10 @@ async function main() {
     `vault address: ${vault.address}`
   )
   console.log(
-    `oracle address: ${oracle.address}`
+    `dataoracle address: ${dataOracle.address}`
+  )
+  console.log(
+    `dataoracle address: ${randomOracle.address}`
   )
   console.log(
     `world address: ${world.address}`
